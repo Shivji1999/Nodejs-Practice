32942b62ed488ce1a3c4a9704260430670186bfb
fire_heart <shivji1999@gmail.com>
2022-08-28 21:49:45 +0530

    Securing passwords using bcriptjs & middleware

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcryptjs = require('bcryptjs')
 
-const User = mongoose.model('User',{
+const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
@@ -29,5 +30,19 @@ const User = mongoose.model('User',{
         default: 0
     }
 })
+
+userSchema.pre('save', async function(next){
+    const user = this
+    console.log('Before Save')
+    if(user.isModified('password')){
+        user.password = await bcryptjs.hash(user.password, 8)
+    }
+
+    next()
+})
+
+const User = mongoose.model('User',userSchema)
+
+
 
 module.exports = User
