a6cc3b5e33f5dac0da3a41db3a3b8c790103820a
fire_heart <shivji1999@gmail.com>
2022-09-13 19:58:39 +0530

    Product update

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -1,13 +1,19 @@
 const express = require('express')
+const http = require('http')
 const path = require('path')
+const socketio = require('socket.io')
 
 const app = express()
 const publicDir = path.join(__dirname, '../public')
-
 app.use(express.static(publicDir))
 
+const server = http.createServer(app)
+const io = socketio(server)
 
+io.on('connection', ()=>{
+    console.log('Setup socket Connection')
+})
 
-app.listen(3000, ()=>{
+server.listen(3000, ()=>{
     console.log('Server Started')
 })
